91dd433ccf813bbf8bc785560212320b3fb64576
ThangMinhCao <thangminhcao@gmail.com>
2022-02-08 18:41:41 -0500

    Remove create db script

--- app/database/connector.py
+++ app/database/connector.py
@@ -5,6 +5,3 @@ def init_database(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
-    with app.app_context():
-        db.create_all()
